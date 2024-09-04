d026e544e063321b5379e4764a1fe685f5b1d436
shanME <giorgie103188@gmail.com>
2024-09-04 18:58:55 +0400

    OPERATOR PRECENDENCE

--- script.js
+++ script.js
@@ -58,3 +58,30 @@
 
 /////////////////////////////////////////////////////////////////////////////////////
 //BASIC OPERAOTORS
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "joans";
+// const lastName = "Shmedtmann";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2019);
+
+/////////////////////////////////////////////////////////////////////////
+//OPERATOR PRECENDENCE
