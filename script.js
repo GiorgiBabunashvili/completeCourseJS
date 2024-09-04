f881994f05f574b8d69b6b2d2ad76ffc61c2acd2
shanME <giorgie103188@gmail.com>
2024-09-04 20:16:34 +0400

    truthly and falsy values

--- script.js
+++ script.js
@@ -186,3 +186,23 @@
 
 ///////////////////////////////////////////////////////////
 //TYPE CONVERSION AND COERCION
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+
+// console.log(inputYear + 18);
+// console.log(Number("jonas"));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
+
+// //type coercion
+// console.log("i am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" * "2");
+// console.log("23" / "2");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+/////////////////////////////////////////////////////////////////
+//truthly and falsy values
