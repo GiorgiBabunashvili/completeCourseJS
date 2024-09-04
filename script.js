b057881bf22c27a657e020fced82322643efcaa2
shanME <giorgie103188@gmail.com>
2024-09-04 18:27:43 +0400

    data types

---
+++ script.js
@@ -0,0 +1,22 @@
+// VALUES AND VARIALBLES
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let myFirstJob = "Programer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+//DATA TYPES
